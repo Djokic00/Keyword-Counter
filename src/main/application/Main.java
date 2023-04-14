6500d34ea86e67db328e6040631e64e6228786ec
Djokic00 <adjokic1619rn@raf.rs>
2023-04-14 14:29:45 +0200

    Implement main

--- src/main/application/Main.java
+++ src/main/application/Main.java
@@ -3,22 +3,22 @@ package main.application;
 import main.crawler.DirectoryCrawler;
 import main.enums.JobStatus;
 import main.enums.ScanType;
-import main.job.Job;
 import main.dispatcher.JobDispatcher;
-import main.result.ResultRetrieverImpl;
+import main.job.ScanningJob;
+import main.job.WebJob;
+import main.result.ResultRetrieverThreadPool;
 import main.scanners.file_scanner.FileScanThreadPool;
 import main.scanners.web_scanner.WebScanThreadPool;
 
 import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.*;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class Main {
     public static void main(String[] args) {
-        var jobQueue = new LinkedBlockingQueue<Job>();
-        BlockingQueue<Future<Map<String, Integer>>> resultQueue = new LinkedBlockingQueue<>();
+        var jobQueue = new LinkedBlockingQueue<ScanningJob>();
 
         Properties props = new Properties();
         try {
@@ -40,12 +40,13 @@ public class Main {
         var directoryCrawlerThread = new Thread(directoryCrawler);
         directoryCrawlerThread.start();
 
-        var resultRetriever = new ResultRetrieverImpl(resultQueue);
-        var resultRetrieverThread = new Thread(resultRetriever);
-        resultRetrieverThread.start();
+        var resultRetriever = new ResultRetrieverThreadPool(url_refresh_time);
 
-        var fileScanThreadPool = new FileScanThreadPool(file_scanning_size_limit, keywords, resultQueue);
-        var webScanThreadPool = new WebScanThreadPool(hop_count, url_refresh_time, keywords);
+        var fileScanThreadPool = new FileScanThreadPool(file_scanning_size_limit, keywords, resultRetriever);
+        var webScanThreadPool = new WebScanThreadPool(jobQueue, resultRetriever, keywords);
+
+        resultRetriever.setWebScanThreadPool(webScanThreadPool);
+        resultRetriever.deleteScannedUrls();
 
         var jobDispatcher = new JobDispatcher(jobQueue, fileScanThreadPool, webScanThreadPool);
         var jobDispatcherThread = new Thread(jobDispatcher);
@@ -56,13 +57,11 @@ public class Main {
 
         while (!shutdown) {
             String userInput = scanner.nextLine();
-            String[] command = userInput.split(" ");
-
-            if (command.length > 2) {
-                System.out.println("Too many arguments, commands have maximum two parameters!");
-            }
-            else switch (command[0]) {
-                case "ad":
+            if (userInput.startsWith("ad")) {
+                String[] command = userInput.split(" ");
+                if (command.length > 2) {
+                    System.out.println("Invalid command. Command ad is of the form: ad name_of_directory");
+                } else {
                     String directoryPath = command[1];
                     File directory = new File(directoryPath);
                     if (!directory.exists()) {
@@ -70,50 +69,98 @@ public class Main {
                     } else if (!directory.isDirectory()) {
                         System.out.println("The file is not a directory. You must specify a path to directory!");
                     }
-
-                    directoryCrawler.setDirectory(directory);
-
-                    break;
-                case "aw":
+                    else directoryCrawler.setDirectory(directory);
+                }
+            }
+            else if (userInput.startsWith("aw")) {
+                String[] command = userInput.split(" ");
+                if (command.length > 2) {
+                    System.out.println("Invalid command. Command aw is of the form : aw url");
+                } else {
+                    String urlString = command[1];
                     try {
-                        jobQueue.put(new Job(ScanType.WEB, command[1], JobStatus.RUNNING));
-                    } catch (InterruptedException e) {
-
+                        new URL(command[1]);
+                        jobQueue.put(new WebJob(ScanType.WEB, urlString, JobStatus.RUNNING, hop_count));
+                    } catch (MalformedURLException | InterruptedException e) {
+                        System.out.println(urlString + " is not a valid URL.");
                     }
-                    break;
-                case "get":
-                    String[] splitCommand = command[1].split("|");
-                    Map<String, Integer> result = resultRetriever.getResult(splitCommand[1]);
+                }
+            }
+            else if (userInput.startsWith("get file|summary")) {
+                Map<String, Map<String, Integer>> result = resultRetriever.getSummary(ScanType.FILE);
+                if (result != null && !result.isEmpty())
+                    System.out.println(result);
+            }
+            else if (userInput.startsWith("query file|summary")) {
+                Map<String, Map<String, Integer>> result = resultRetriever.querySummary(ScanType.FILE);
+                if (result != null && !result.isEmpty())
+                    System.out.println(result);
+            }
+            else if (userInput.startsWith("get file|")) {
+                String directoryPath = userInput.split("\\|")[1];
+                Map<String, Integer> result = resultRetriever.getResult("file|" + directoryPath);
+                if (!result.isEmpty()) {
                     for (Map.Entry<String, Integer> entry : result.entrySet()) {
                         System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
                     }
-                    break;
-                case "query":
-
-                    break;
-                case "cfs":
-                    break;
-                case "cws":
-                    break;
-                case "stop":
-                    System.out.println("Stopping...");
-                    shutdown = true;
-                    directoryCrawler.stopThread();
-                    jobDispatcher.stopThread();
-
-                    try {
-                        directoryCrawlerThread.join();
-                        jobDispatcherThread.join();
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
+                }
+            }
+            else if (userInput.startsWith("query file|")) {
+                String directoryPath = userInput.split("\\|")[1];
+                Map<String, Integer> result = resultRetriever.getQueryResult("file|" + directoryPath);
+                if (!result.isEmpty()) {
+                    for (Map.Entry<String, Integer> entry : result.entrySet()) {
+                        System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
                     }
-                    break;
-                default:
-                    System.out.println("Command does not exists!");
+                }
+            }
+            else if (userInput.startsWith("get web|summary")) {
+                Map<String, Map<String, Integer>> result = resultRetriever.getSummary(ScanType.WEB);
+                if (result != null && !result.isEmpty())
+                    System.out.println(result);
+            }
+            else if (userInput.startsWith("query web|summary")) {
+                Map<String, Map<String, Integer>> result = resultRetriever.querySummary(ScanType.WEB);
+                if (result != null && !result.isEmpty())
+                    System.out.println(result);
+            }
+            else if (userInput.startsWith("get web|")) {
+                String url = userInput.split("\\|")[1];
+                Map<String, Integer> result = resultRetriever.getResult("web|" + url);
+                if (!result.isEmpty()) {
+                    System.out.println(result);
+                }
+            }
+            else if (userInput.startsWith("query web|")) {
+                String url = userInput.split("\\|")[1];
+                Map<String, Integer> result = resultRetriever.getQueryResult("web|" + url);
+                if (!result.isEmpty()) {
+                    System.out.println(result);
+                }
+            }
+            else if (userInput.startsWith("cfs")) {
+                resultRetriever.clearSummary(ScanType.FILE);
+            }
+            else if (userInput.startsWith("cws")) {
+                resultRetriever.clearSummary(ScanType.WEB);
+            }
+            else if (userInput.equals("stop")) {
+                System.out.println("Stopping...");
+                shutdown = true;
+                directoryCrawler.stopThread();
+                jobDispatcher.stopThread();
+
+                try {
+                    directoryCrawlerThread.join();
+                    jobDispatcherThread.join();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                resultRetriever.stopPool();
+            }
+            else {
+                System.out.println("Command does not exists");
             }
         }
     }
 }
-//ad src/testcases/data
-//ad src/testcases/data2
-// aw
